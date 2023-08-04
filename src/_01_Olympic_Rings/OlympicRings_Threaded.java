c837fefd34f04a7de8f65b8f33fad5e9dd1270fd
student <student@ip-172-31-62-102.us-west-2.compute.internal>
2023-08-03 21:44:08 -0700

    robot

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,37 @@
 package _01_Olympic_Rings;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
-	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+	// Make A Program that uses Threads and robots to draw the Olympic rings. One
+	// robot should draw one ring simultaneously with the other 4 robots.
 
+	public static void main(String[] args) {
+
+		Robot rob1 = new Robot(400, 700);
+		Robot rob2 = new Robot(600, 700);
+		Robot rob3 = new Robot(800, 700);
+		Robot rob4 = new Robot(1000, 700);
+		Robot rob5 = new Robot(1200,700);
+		Graphics2D g = null;
+		g.drawOval(100, 100, 100, 100);
+		
+		Robot robo[] = {rob1, rob2, rob3, rob4, rob5};
+		for(Robot r : robo) {
+			r.penDown();
+			r.setRandomPenColor();
+			Thread r1 = new Thread(() -> r.draw(g));
+			r1.start();
+		}
+		
+	
+
+		
+
+		
+	}
 }
-
--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,66 @@
 package _02_Advanced_Robot_Race;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class AdvancedRobotRace {
-	// Re-do the robot race recipe from level 3 module 0. 
+	// Re-do the robot race recipe from level 3 module 0.
 	// This time, use threads to make all of the robots go at the same time.
+
+	// 1. make a main method
+	public static void main(String[] args) {
+		Robot[] robo = new Robot[10];
+
+		for (int i = 0; i < robo.length; i++) {
+			robo[i] = new Robot();
+			robo[i].setY(460);
+			robo[i].setSpeed(10);
+		}
+		robo[0].setX(100);
+		robo[1].setX(200);
+		robo[2].setX(300);
+		robo[3].setX(400);
+		robo[4].setX(500);
+		robo[5].setX(600);
+		robo[6].setX(650);
+		robo[7].setX(700);
+		robo[8].setX(750);
+		robo[9].setX(800);
+
+		
+		Random E = new Random();
+		for(int i = 0; i < robo.length; i++) {
+			while(robo[i].getY() > 30) {
+				Thread t = new Thread(()-> robo[i].move(E.nextInt(50)));
+			}
+		}
+	
+		for (int e = 0; e < robo.length; e++) {
+			int f = robo[e].getY();
+			if (f < 30) {
+				System.out.println(f + " is the winner");
+			}
+		}
+
+// 2. create an array of 5 robots.
+
+		// 3. use a for loop to initialize the robots.
+
+		// 4. make each robot start at the bottom of the screen, side by side, facing up
+
+		// 5. use another for loop to iterate through the array and make each robot move
+		// a random amount less than 50.
+
+		// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+		// screen.
+
+		// 7. declare that robot the winner and throw it a party!
+
+		// 8. try different races with different amounts of robots.
+
+		// 9. make the robots race around a circular track.
+	}
 }
